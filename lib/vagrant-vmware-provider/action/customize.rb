464ba13dc90618ce4a8b47bd096372c2a243cbeb
Steve Jabour <steve@jabour.me>
2015-12-03 09:13:12 -0700

    Argument error resolve with Vagrant 1.7.4

--- lib/vagrant-vmware-provider/action/customize.rb
+++ lib/vagrant-vmware-provider/action/customize.rb
@@ -2,13 +2,13 @@ module VagrantPlugins
   module VMwareProvider
     module Action
       class Customize
-        def initialize(app, env, event)
+        def initialize(app, env, event="")
           @app = app
           @event = event
         end
 
         def call(env)
-          env[:ui].info I18n.t("vagrant.actions.vm.customize.running", event)
+          env[:ui].info I18n.t("vagrant.actions.vm.customize.running", @event)
           env[:machine].provider_config.vmx.each do |key, value|
             env[:machine].provider.driver.customize(key, value)
           end
